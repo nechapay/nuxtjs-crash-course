19b986d6d74b8e4a1acf5905a16eb9fb5d8b5732
pavel <nechapay@list.ru>
2021-10-19 12:46:07 +0300

    first commit

---
+++ components/LargeCardDisplay.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="container">
+    <h4 class="header">{{ cardSection.title }}</h4>
+    <p class="snippet">{{ cardSection.spippet }}</p>
+    <div class="cards-container">
+      <LargeCard v-for="card in cardSection.cards" :key="card.image" :card="card" />
+    </div>
+  </div>
+</template>
+
+<script>
+import LargeCard from "./LargeCard.vue"
+export default {
+  components: { LargeCard },
+  props: ['cardSection']
+}
+</script>
+
+<style scoped>
+.container {
+  margin-top: 2rem;
+}
+.header {
+  font-weight: 700;
+  font-size: 1.5rem;
+}
+.snippet {
+  color: grey;
+  margin-bottom: 1.5rem;
+}
+.cards-container {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="containner">
+    <Hero />
+    <LargeCardDisplay v-for="cardInfo in largeCardInfo" :key="cardInfo.id" :cardSection="cardInfo" />
+    <SmallCardDisaplay v-for="cardInfo in smallCardInfo" :key="cardInfo.id" :cardSection="cardInfo" />
+  </div>
+</template>
+
+<script>
+import Hero from '../components/Hero.vue'
+import LargeCardDisplay from '../components/LargeCardDisplay.vue'
+import { largeCardSections, smallCardSections } from '@/assets/data.js'
+import SmallCardDisaplay from '../components/SmallCardDisaplay.vue'
+
+export default {
+  components: { Hero, LargeCardDisplay, SmallCardDisaplay },
+  data() {
+    return {
+      largeCardInfo: largeCardSections,
+      smallCardInfo: smallCardSections
+    }
+  }
+}
+</script>
